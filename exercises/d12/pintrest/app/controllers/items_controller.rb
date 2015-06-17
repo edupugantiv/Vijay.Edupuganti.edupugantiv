370e47f01f1f39f966fb95ba67c0874a9da2908d
edupugantiv <vijayedupuganti@yahoo.com>
2015-06-17 14:57:26 +0200

    pintrest updated

--- exercises/d12/pintrest/app/controllers/items_controller.rb
+++ exercises/d12/pintrest/app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
+	def new
+		@item = Item.new
+	end
+
 	def index
+		@items = Item.all
+	end
+	
+	def create
+		@item = Item.new(params["item"].permit(:title, :description, :user_id))
+		@item.save
+		redirect_to '/items'
 	end
 end
--- exercises/d12/pintrest/config/routes.rb
+++ exercises/d12/pintrest/config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  # root 'items#index'
+  root 'items#index'
 
   # Example of regular route:
   get 'items' => 'items#index'
+
+  get 'items/new' => 'items#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
