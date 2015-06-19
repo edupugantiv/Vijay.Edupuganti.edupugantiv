924976383b357f7b8f244a8b1a0ddbfeb5443633
edupugantiv <vijayedupuganti@yahoo.com>
2015-06-19 11:59:07 +0200

    devise functional

--- exercises/d12/pintrest/app/controllers/items_controller.rb
+++ exercises/d12/pintrest/app/controllers/items_controller.rb
@@ -6,16 +6,21 @@ class ItemsController < ApplicationController
 		@item = Item.new
 	end
 
+	def show
+		@item = Item.find(params[:id])
+	end
+
 	def index
 		@items = Item.all
 	end
 	
 	def create
 		@item = Item.new(params["item"].permit(:title, :description, :user_id))
+		@item.save
 		if @item.save
 			redirect_to '/items', notice: "Item created"
 		else
-			redirect_to '/items', notice: "Item not created"
+			render :new, notice: "Item not created"
 		end
 	end
 end
